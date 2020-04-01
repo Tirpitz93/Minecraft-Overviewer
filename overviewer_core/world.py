3abb8c9f9e25954f3aba16ee2e1eac928311b416
Jens <jens.winkle@gmx.de>
2020-04-01 15:29:37 +0200

    textures.py: Remove beehive, bee_nest

--- overviewer_core/world.py
+++ overviewer_core/world.py
@@ -837,8 +837,6 @@ class RegionSet(object):
             'minecraft:bamboo': (11416, 0),
             "minecraft:composter": (11417, 0),
             # 1.15 blocks below
-            'minecraft:beehive': (11501, 0),
-            'minecraft:bee_nest': (11502, 0),
             # adding a gap in the numbering of walls to keep them all
             # in one numbering block starting at 21000
             'minecraft:andesite_wall': (21000, 0),
@@ -1029,22 +1027,6 @@ class RegionSet(object):
         elif key in ['minecraft:ladder', 'minecraft:chest', 'minecraft:ender_chest', 'minecraft:trapped_chest', 'minecraft:furnace']:
             facing = palette_entry['Properties']['facing']
             data = {'north': 2, 'south': 3, 'west': 4, 'east': 5}[facing]
-        elif key in ['minecraft:beehive', 'minecraft:bee_nest']:
-            facing = palette_entry['Properties']['facing']
-            honey_level = int(palette_entry['Properties']['honey_level'])
-            data = {'south': 0, 'west': 1, 'north': 2, 'east': 3}[facing]
-            if honey_level == 5:
-                data = {'south': 4, 'west': 5, 'north': 6, 'east': 7}[facing]
-        # elif key.endswith('_button'):
-        #     facing = palette_entry['Properties']['facing']
-        #     face   = palette_entry['Properties']['face']
-        #     if face == 'ceiling':
-        #         block = 0
-        #         data = 0
-        #     elif face == 'wall':
-        #         data = {'east': 1, 'west': 2, 'south': 3, 'north': 4}[facing]
-        #     elif face == 'floor':
-        #         data = {'east': 6, 'west': 6, 'south': 5, 'north': 5}[facing]
         elif key == 'minecraft:nether_wart':
             data = int(palette_entry['Properties']['age'])
         elif key.endswith('shulker_box') or key.endswith('piston') or key in ['minecraft:observer', 'minecraft:dropper', 'minecraft:dispenser']:
